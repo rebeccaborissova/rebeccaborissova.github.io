0c44a71a88ff578a2b13d4f167b8215b5a967ea9
Rebecca <147621579+rebeccaborissova@users.noreply.github.com>
2024-01-05 00:33:51 -0700

    Add files via upload
    mostly complete

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirectToMinesweeper() {
+    window.location.href = 'minesweeper.html'; // Replace with the actual URL
+}
+
+function redirectToBlackjack() {
+    window.location.href = 'blackjackstatistics.html'; // Replace with the actual URL
+}
+
+function redirectToTruckInsuranceScraper() {
+    window.location.href = 'truckinsurancescraper.html'; // Replace with the actual URL
+}
+
+function goHome() {
+    window.location.href = "index.html";
+}
